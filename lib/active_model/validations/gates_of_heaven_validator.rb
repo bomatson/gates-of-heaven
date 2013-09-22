a1399926faad4eb8dfbf3b12c8ae3c99b925d9b9
Giles Bowkett <gilesb@gmail.com>
2013-09-22 13:39:29 -0700

    essay homework!

--- lib/active_model/validations/gates_of_heaven_validator.rb
+++ lib/active_model/validations/gates_of_heaven_validator.rb
@@ -3,12 +3,14 @@ require 'gates_of_heaven'
 module ActiveModel
   module Validations
     class GatesOfHeavenValidator < ActiveModel::EachValidator
+
       def validate_each(record, attribute, value)
         error_message = GatesOfHeaven.validate(record.password)
-        if error_message
+        unless :awesome == error_message
           record.errors.add(:password, error_message)
         end
       end
+
     end
   end
 end
--- lib/gates_of_heaven.rb
+++ lib/gates_of_heaven.rb
@@ -1,15 +1,30 @@
 require "active_model/validations"
 require "gates_of_heaven/version"
-require "gates_of_heaven/doorman"
 require "active_model/validations/gates_of_heaven_validator" if defined? ActiveModel
 
 module GatesOfHeaven
+  def self.validate(password)
+    case
+    when password.length <= 6
+      'Password too short'
+    when !password.match(/\d/)
+      'Password needs numbers'
+    else
+      :awesome
+    end
+  end
 end
- # proposed_password = "something"
-#  h = Hash.new
- # h.default_proc = proc do |proposed_password|
-  # proposed_password,gsub(/^[a-zA-Z]\w{3,14}$/)
-  #  PasswordChecker.check(proposed_password)
- # end
 
-  #h[username] = proposed_password
+# essay homework:
+
+# watch https://www.destroyallsoftware.com/screencasts/catalog/how-and-why-to-avoid-nil
+
+# and maybe https://www.destroyallsoftware.com/screencasts/catalog/exceptions-and-control-flow
+
+# write essay and bring to class
+
+# essay covers:
+# what you saw (book report style)
+# what you think, and why
+# how it applies to the code in this file
+
